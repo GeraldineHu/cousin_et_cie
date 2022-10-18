edb66c33c4cf4bd70a45dac2a4cade8af3957c4c
Geraldine Hurteau <geraldine.hurteau@gmail.com>
2022-10-18 09:41:40 +0200

    creation page eshop

---
+++ header.php
@@ -0,0 +1,20 @@
+<header class="flexContainer flexColumn">
+        <nav class="flexContainer">
+            <ul>
+                <li><a href="">À propos</a></li>
+                <li><a href="">Les dégustations</a></li>
+            </ul>
+            <p class="textLogo">Cousin & Cie</p>
+            <ul>
+                <li><a href="">La boutique</a></li>
+                <li><a href="">Les actualités</a></li>
+            </ul>
+        </nav>
+        <div class="flexContainer flexColumn">
+            <h1>Cousin & Compagnie</h1>
+            <h2>Caviste au naturel, depuis 1993</h2>
+            <div class="btn">
+                <a href="">La boutique en ligne</a>
+            </div>
+        </div>    
+    </header>
